a50019405052af34167ae1ec961c3b0adfdb5493
sleeperops <>
2026-06-24 17:06:04 +0800

    Feat: Made the load and transfer functions as part of the memory class; Fix issues with not loading two or more process

--- MVT.py
+++ MVT.py
@@ -9,8 +9,15 @@ class Process:
         self.size = size
         self.start = start
 
+def transfer(memory, process):
+    print("Transfer in progress...")
+    for i in range(process.size):
+        print(f"Current block: memory[{i}] = {memory.array[i+process.start]}")
+        memory.array[i + process.start] += 1
+        print(f"    -> memory[{i}] = {memory.array[i+process.start]}")
+
 # checks if memory section is available 
-def load(memory, process):
+def load(memory, process):  #!put the start parameter here 
     target_end= process.start + process.size
     if target_end > memory.size:  #Either process is too big or external fragmentation
         return "cannot be"  # process too big
@@ -18,30 +25,29 @@ def load(memory, process):
     else:  # check if target section is free
         for address, block in enumerate(memory.array):  # address is the index and block is the value inside that index
             if block == 0:
-                print(f"{memory}[{address}] = {block}  // OK")
+                print(f"{memory}[{address}] = {block}  // OK") # ! add return
                 
             elif block != 0:
-                print(f"{memory}[{address}] = {block}  // OCCUPIED !")
+                print(f"{memory}[{address}] = {block}  // OCCUPIED !") #!add return
                 break
         else:
             print("Can load blocks")
-            print("Memory section is available")
+            print("Memory section is available") #!add return
             
             # transfer
-def transfer(memory, process, target_start, target_end):
-    for i in range(process.size):
-        memory.array[i + target_start] += 1
-        
-    
+            transfer(memory,process)
 
-                
+def display_memory(memory):
+    for address, block in enumerate(memory.array):
+        print(f"memory[{address}] = {block}")
         
 memory_1 = Memory(32, 0) # 0-31, !Make 32 a variable
-p1 = Process("A", 16, 0)
+p1 = Process("A", 3, 30)
 
-memory_1.array[4] = 1
+memory_1.array[4] = 0
 
 print(load(memory_1, p1))
 print(memory_1.array[0])
 print(memory_1.array[4])
 
+display_memory(memory_1)
