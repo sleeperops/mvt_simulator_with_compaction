cdbd59b2830f65448fe2f8344cee808a941f90eb
sleeperops <>
2026-06-24 12:59:49 +0800

    Add memory class

--- MVT.py
+++ MVT.py
@@ -1,8 +1,17 @@
+class Memory:
+    def __init__(self,size, fill):
+        self.size = size
+        self.fill = fill
+        self.array = [fill] * size
 class Process:
     def __init__(self, name, size, start):
         self.name = name
         self.size = size
         self.start = start
+    
+
         
-memory = [0] * 32  # !Make 32 a variable
+        
+memory_1 = Memory(32, 0) # !Make 32 a variable
+print(memory_1.array[23])
 
